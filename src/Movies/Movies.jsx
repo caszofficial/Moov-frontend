bee57ee6db695df7b7cedef08563ae56a0e9f614
Santiago <santiagozapata07@gmail.com>
2025-07-21 18:07:21 -0500

    ordering cards

--- src/Movies/Movies.jsx
+++ src/Movies/Movies.jsx
@@ -5,43 +5,58 @@ import useGetMovies from "../hooks/useGetMovies";
 const Movies = ({ movies, moviesAPI }) => {
   return (
     <Container title="Top Worldwide movies">
-      {movies?.map((movie) => (
-        <div
-          className="bg-white border-0 rounded-lg w-50 flex flex-col items-center justify-around shadow-[inset_0px_0px_3px_#000]"
-          key={movie._id}
-        >
-          <h1>{movie.title}</h1>
-          <p>{movie.rating}</p>
-          <p>{movie.releaseDate}</p>
+      <div className="grid grid-cols-1 gap-5">
+        <div className="grid grid-cols-5 gap-5 justify-items-center content-center">
+          {movies?.map((movie) => (
+            <div
+              className="min-w-[250px] max-w-[250px] bg-white rounded-lg shadow-md"
+              key={movie._id}
+            >
+              <div className="w-full h-[400px] overflow-hidden rounded-t-lg">
+                <img
+                  src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                  alt={movie.title || movie.name}
+                  className="w-full h-full object-fill"
+                />
+              </div>
+              <div className="p-2 text-center">
+                <h2 className="font-semibold text-sm">{movie.title}</h2>
+                <p className="font-semibold text-xs">{movie.rating}</p>
+                <p className="text-xs text-gray-500">{movie.releaseDate}</p>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
-      {moviesAPI?.map((movie) => {
-        return (
-          <div
-            key={movie.id}
-            className=" min-w-[250px] max-w-[250px] bg-white rounded-lg shadow-md"
-          >
-            {/* 1) Contenedor de la imagen con alto fijo */}
-            <div className="w-full h-[400px] overflow-hidden rounded-t-lg">
-              <img
-                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                alt={movie.title || movie.name}
-                className="w-full h-full object-fill"
-              />
-            </div>
+        <div className="grid grid-cols-5 content-center justify-items-center gap-5">
+          {moviesAPI?.map((movie) => {
+            return (
+              <div
+                key={movie.id}
+                className=" min-w-[250px] max-w-[250px] bg-white rounded-lg shadow-md"
+              >
+                {/* 1) Contenedor de la imagen con alto fijo */}
+                <div className="w-full h-[400px] overflow-hidden rounded-t-lg">
+                  <img
+                    src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                    alt={movie.title || movie.name}
+                    className="w-full h-full object-fill"
+                  />
+                </div>
 
-            {/* 2) Pie de tarjeta */}
-            <div className="p-2 text-center">
-              <h2 className="font-semibold text-sm">
-                {movie.title || movie.name}
-              </h2>
-              <p className="text-xs text-gray-500">
-                {movie.release_date || movie.first_air_date}
-              </p>
-            </div>
-          </div>
-        );
-      })}
+                {/* 2) Pie de tarjeta */}
+                <div className="p-2 text-center">
+                  <h2 className="font-semibold text-sm">
+                    {movie.title || movie.name}
+                  </h2>
+                  <p className="text-xs text-gray-500">
+                    {movie.release_date || movie.first_air_date}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </Container>
   );
 };
--- src/components/ui/Container.jsx
+++ src/components/ui/Container.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Container = ({ children, button, title }) => {
   return (
-    <div className="flex flex-col bg-white min-w-8/10 min-h-full border-1 border-gray-300 rounded-xl ">
+    <div className="flex flex-col bg-white min-w-8/10 min-h-full border-1 border-gray-300 rounded-xl overflow-auto">
       <div className="flex justify-between items-center w-full px-10 h-20">
         <p className="text-2xl font-bold">{title}</p>
         {button && (
